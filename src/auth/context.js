f6716ab2099b2858e0cadca3bcedea11f1a56bd4
Ceowyllian <downwell@online.ua>
2025-01-06 10:26:49 +0300

    freat(auth): add a context storing info about the user

---
+++ src/auth/context.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState({
+    "username": null,
+  });
+
+  return (
+    <AuthContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
