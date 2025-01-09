2b87a18c80f586bb84ebb73fd88a2dcc1901f8fc
Ceowyllian <downwell@online.ua>
2025-01-09 09:38:49 +0300

    feat(auth): update user panel based on authentication status

--- src/components/TopNavigation/UserBar.jsx
+++ src/components/TopNavigation/UserBar.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { getUser, isUserAuthenticated, removeUser } from '../../auth/';
+import { logout } from '../../api/auth';
 
 export default function UserBar() {
-  return (
-    <Button as={NavLink} to={'login/'}>
-      <span className={'bi-person me-1'} />
-      <span className={'d-none d-sm-inline'}>Sign-in</span>
-    </Button>
+  const user = getUser();
+  const navigate = useNavigate();
+
+  async function logoutSubmit() {
+    const response = await logout();
+    removeUser();
+    navigate('/');
+  }
+
+  return isUserAuthenticated() ? (
+    <>
+      <Button as={NavLink} to={'profile/'}>
+        <span className={'bi-person me-1'} />
+        <span className={'d-none d-sm-inline'}>{user.username}</span>
+      </Button>
+      <Button className={'ms-1 bg-transparent border-0'} onClick={logoutSubmit}>
+        <span className={'bi-arrow-return-right me-1'} />
+        <span className={'d-none d-sm-inline'}>Exit</span>
+      </Button>
+    </>
+  ) : (
+    <>
+      <Button
+        as={NavLink}
+        to={'login/'}
+        className={'bg-transparent border-white'}
+      >
+        Sign-in
+      </Button>
+      <Button as={NavLink} to={'registration/'} className={'ms-1'}>
+        Create account
+      </Button>
+    </>
   );
 }
