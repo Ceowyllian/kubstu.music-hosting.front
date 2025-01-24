0d6722ca4d5b2394bcbc84615df3cff12c423898
Ceowyllian <downwell@online.ua>
2025-01-24 21:34:50 +0300

    feat: add an input component

---
+++ src/components/forms/Input.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+function Input({state, label, type, required = false}) {
+  const {
+    value,
+    errorText,
+    onChange,
+  } = state;
+  return (
+    <Form.Group>
+      <Form.Label>
+        {label}
+      </Form.Label>
+      <Form.Control
+        onChange={onChange}
+        value={value}
+        type={type}
+        required={required}
+        isInvalid={!!errorText}
+      />
+      {!!errorText && <Form.Control.Feedback type={"invalid"}>
+        {errorText}
+      </Form.Control.Feedback>}
+    </Form.Group>
+  );
+}
+
+export default Input;
