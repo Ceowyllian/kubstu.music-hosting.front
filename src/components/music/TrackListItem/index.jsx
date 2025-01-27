c6d24b1f2faebf95ca31b5079d8795593140c962
Ceowyllian <downwell@online.ua>
2025-01-27 23:36:51 +0300

    feat(music): replace property pass-through with context

--- src/components/music/TrackListItem/index.jsx
+++ src/components/music/TrackListItem/index.jsx
@@ -1,21 +1,26 @@
-import { PlayButton, TrackDuration } from 'components/music';
-import { useAudio } from 'hooks/music';
+import {
+  TogglePlayButton,
+  LoadTrackButton,
+  TrackDuration,
+  TrackProgressBar,
+} from 'components/music';
+import { usePlayerContext } from 'hooks/music';
 import React from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function TrackListItem({ track }) {
-  const [state, controls, audioRef] = useAudio(track.sound_file);
+  const { state, isPlaying } = usePlayerContext();
 
   return (
     <Row className={`p-0`}>
       <Col md={1} xs={2}>
         <div className={'p-1 m-1'}>
-          <PlayButton
-            isPlaying={state.isPlaying}
-            play={controls.play}
-            pause={controls.pause}
-          />
+          {isPlaying(track) ? (
+            <TogglePlayButton />
+          ) : (
+            <LoadTrackButton track={track} />
+          )}
         </div>
       </Col>
       <Col className={'ms-2 me-2'}>
@@ -24,23 +29,17 @@ function TrackListItem({ track }) {
             <Link to={`/tracks/${track.id}`}>{track.title}</Link>
           </Col>
           <Col xs={6} sm={5} md={3} xxl={2}>
-            <TrackDuration
-              currentTime={state.currentTime}
-              duration={state.duration}
-            />
+            {isPlaying(track) ? (
+              <TrackDuration
+                currentTime={state.currentTime}
+                duration={state.duration}
+              />
+            ) : (
+              '0:0 / 0:00'
+            )}
           </Col>
         </Row>
-        <Row>
-          <Form.Range
-            min={0}
-            max={state.duration}
-            defaultValue={0}
-            value={state.currentTime}
-            onInput={(e) => {
-              controls.seek(e.target.value);
-            }}
-          />
-        </Row>
+        <Row>{isPlaying(track) && <TrackProgressBar />}</Row>
       </Col>
     </Row>
   );
--- src/pages/my-tracks/index.jsx
+++ src/pages/my-tracks/index.jsx
@@ -1,6 +1,7 @@
 import { myTracks } from 'api/my';
 import { TrackListItem } from 'components/music/';
 import { useFetch } from 'hooks';
+import { PlayerProvider } from 'hooks/music';
 import React, { useEffect, useState } from 'react';
 import { Container, ListGroup } from 'react-bootstrap';
 
@@ -16,13 +17,15 @@ function MyTracks() {
   return (
     <Container fluid className={'d-flex justify-content-center'}>
       <ListGroup className={'w-50'}>
-        {tracks.map((track) => {
-          return (
-            <ListGroup.Item key={track.id} className={'p-0 m-1 border-2'}>
-              <TrackListItem track={track}/>
-            </ListGroup.Item>
-          );
-        })}
+        <PlayerProvider>
+          {tracks.map((track) => {
+            return (
+              <ListGroup.Item key={track.id} className={'p-0 m-1 border-2'}>
+                <TrackListItem track={track} />
+              </ListGroup.Item>
+            );
+          })}
+        </PlayerProvider>
       </ListGroup>
     </Container>
   );
